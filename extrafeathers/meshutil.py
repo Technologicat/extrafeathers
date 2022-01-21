fc50971dae8f948ca2f588e833253991b8aaa783
Juha Jeronen <juha.m.jeronen@gmail.com>
2022-01-21 13:22:01 +0200

    add write_hdf5_mesh; clean up

--- extrafeathers/meshutil.py
+++ extrafeathers/meshutil.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8; -*-
 """Gmsh import for FEniCS, built on top of meshio.
 
+We also provide convenient functions to read/write a mesh with subdomain and boundary data.
+
 Based on many posts in these discussion threads:
   https://fenicsproject.discourse.group/t/converting-simple-2d-mesh-from-gmsh-to-dolfin/583/4
   https://fenicsproject.discourse.group/t/transitioning-from-mesh-xml-to-mesh-xdmf-from-dolfin-convert-to-meshio/412/9
 """
 
-__all__ = ["import_gmsh", "read_hdf5_mesh"]
+__all__ = ["read_hdf5_mesh", "write_hdf5_mesh", "import_gmsh"]
 
 import pathlib
 import tempfile
@@ -18,10 +20,9 @@ import meshio
 
 import dolfin
 
-# names for the datasets in the output files
-mesh_dataset = "mesh"
-cell_dataset = "domain_parts"
-boundary_dataset = "boundary_parts"
+mesh_dataset_name = "mesh"
+cell_dataset_name = "domain_parts"
+boundary_dataset_name = "boundary_parts"
 
 def _absolute_path(filename_or_path: typing.Union[pathlib.Path, str]) -> pathlib.Path:
     return pathlib.Path(filename_or_path).expanduser().resolve()
@@ -33,36 +34,94 @@ def _stack_cells(msh: meshio.Mesh, type: str):
     Some mesh files may have several cell blocks of the same type,
     but FEniCS expects just one.
 
-    `type`: e.g. "line", "triangle", or "tetra".
+    `type`: "line", "triangle", or "tetra".
     """
     cells = np.vstack(np.array([block.data for block in msh.cells
                                 if block.type == type]))
     return cells
 
 
+def read_hdf5_mesh(filename: str) -> typing.Tuple[dolfin.Mesh, dolfin.MeshFunction, dolfin.MeshFunction]:
+    """Read an HDF5 mesh file created using `write_hdf5_mesh` or `import_gmsh`.
+
+    The return value is `(mesh, domain_parts, boundary_parts)`.
+
+    Raises `ValueError` if the file does not have a "/mesh" dataset.
+
+    The "/domain_parts" and "/boundary_parts" datasets are optional.
+    If the file does not have them, the corresponding component of
+    the return value will be `None`.
+    """
+    filename = str(_absolute_path(filename))
+
+    with dolfin.HDF5File(dolfin.MPI.comm_world, filename, "r") as hdf:
+        if not hdf.has_dataset(mesh_dataset_name):
+            raise ValueError(f"{mesh_dataset_name} dataset not found in mesh file {filename}")
+
+        mesh = dolfin.Mesh()
+        hdf.read(mesh, mesh_dataset_name, False)  # target_object, data_path_in_hdf, use_existing_partitioning_if_any
+
+        if hdf.has_dataset(cell_dataset_name):
+            # For the tags, we must specify which mesh the MeshFunction belongs to,
+            # and the function's cell dimension.
+            domain_parts = dolfin.MeshFunction('size_t', mesh, mesh.topology().dim(), 0)  # type, mesh, dim, [default_value]
+            hdf.read(domain_parts, cell_dataset_name)
+        else:
+            domain_parts = None
+
+        if hdf.has_dataset(boundary_dataset_name):
+            boundary_parts = dolfin.MeshFunction('size_t', mesh, mesh.topology().dim() - 1, 0)
+            hdf.read(boundary_parts, boundary_dataset_name)
+        else:
+            boundary_parts = None
+
+    return mesh, domain_parts, boundary_parts
+
+
+def write_hdf5_mesh(filename: str,
+                    mesh: dolfin.Mesh,
+                    domain_parts: typing.Optional[dolfin.MeshFunction] = None,
+                    boundary_parts: typing.Optional[dolfin.MeshFunction] = None) -> None:
+    """Write a mesh and its associated subdomain/boundary mesh functions into an HDF5 file.
+
+    The optional mesh functions should be a `MeshFunction` of type `size_t`.
+    `domain_parts` should be defined on the cells of `mesh`;
+    `boundary_parts` should be defined on the facets of `mesh`.
+    These are meant for physical tagging of subdomains and boundaries.
+
+    The output is a single HDF5 file with three datasets:
+      - "/mesh" contains the mesh itself.
+      - "/domain_parts" (if specified) contains the physical cells
+        (i.e. surfaces in 2D, volumes in 3D).
+      - "/boundary_parts" (if specified) contains the physical facets
+        (i.e. lines in 2D, surfaces in 3D).
+    """
+    filename = str(_absolute_path(filename))
+
+    with dolfin.HDF5File(mesh.mpi_comm(), filename, "w") as hdf:
+        hdf.write(mesh, mesh_dataset_name)
+        if domain_parts:
+            hdf.write(domain_parts, cell_dataset_name)  # MeshFunction on cells of `mesh`
+        if boundary_parts:
+            hdf.write(boundary_parts, boundary_dataset_name)  # MeshFunction on facets of `mesh`
+
+
 def import_gmsh(src: typing.Union[pathlib.Path, str],
                 dst: typing.Union[pathlib.Path, str]) -> None:
     """Import a Gmsh mesh into FEniCS.
 
-    Physical boundaries are also imported.
+    Physical cells (volumes in 3D, surfaces in 2D) and facets (surfaces in 3D, lines in 2D)
+    are also imported.
 
     Simplicial meshes (triangles/tetrahedrons) only.
 
     `src`: The `.msh` file to import.
     `dst`: The `.h5` file to write.
 
-    The output is a single HDF5 file with three datasets:
-      - "/mesh" contains the mesh itself
-      - "/domain_parts" contains the physical cells
-        (i.e. surfaces in 2D, volumes in 3D), if the
-        input file specifies any.
-      - "/boundary_parts" contains the physical facets
-        (i.e. lines in 2D, surfaces in 3D), if the
-        input file specifies any.
+    If the same cell or facet has multiple physical tags, the latest one wins.
 
-    If the same cell or facet has multiple tags, the latest one wins.
-
-    The resulting HDF5 file can be read back in using `read_hdf5_mesh`.
+    On the output format, see `write_hdf5_mesh`.
+    The file can be read back in using `read_hdf5_mesh`.
     """
     if dolfin.MPI.comm_world.size > 1:
         raise NotImplementedError("`import_gmsh` does not support running in parallel. Perform the mesh file conversion serially, then run your solver in parallel.")
@@ -98,7 +157,7 @@ def import_gmsh(src: typing.Union[pathlib.Path, str],
                                  cells={cell_kind: _stack_cells(msh, cell_kind)}),
                      file_format="xdmf")
 
-        # Read the temporary XDMF into FEniCS internal format.
+        # Read the temporary XDMF into FEniCS.
         #
         # XDMFFile is a wrapper for C++ code so it's safe to assume it
         # doesn't support Python filelikes. So pass in the filename here too...
@@ -109,10 +168,11 @@ def import_gmsh(src: typing.Union[pathlib.Path, str],
 
     # Convert Gmsh physical facets (lines in 2D, surfaces in 3D) to XDMF
     with tempfile.NamedTemporaryFile() as boundary_parts_out:
+        # TODO: Can it happen that there are multiple blocks? Do we need to stack something?
         meshio.write(boundary_parts_out.name,
                      meshio.Mesh(points=msh.points,
                                  cells={facet_kind: msh.cells_dict[facet_kind]},
-                                 cell_data={boundary_dataset: [msh.cell_data_dict["gmsh:physical"][facet_kind]]}),
+                                 cell_data={boundary_dataset_name: [msh.cell_data_dict["gmsh:physical"][facet_kind]]}),
                      file_format="xdmf")
 
         # MeshValueCollection represents imported data, which can be
@@ -124,27 +184,25 @@ def import_gmsh(src: typing.Union[pathlib.Path, str],
         # https://fenicsproject.discourse.group/t/transitioning-from-mesh-xml-to-mesh-xdmf-from-dolfin-convert-to-meshio/412/35
         mvc = dolfin.MeshValueCollection("size_t", mesh, mesh.topology().dim() - 1)
         with dolfin.XDMFFile(boundary_parts_out.name) as boundary_parts_in:
-            boundary_parts_in.read(mvc, boundary_dataset)
-        mf = dolfin.MeshFunction("size_t", mesh, mvc)
+            boundary_parts_in.read(mvc, boundary_dataset_name)
+        boundary_parts = dolfin.MeshFunction("size_t", mesh, mvc)
 
     # Convert Gmsh physical cells (surfaces in 2D, volumes in 3D) to XDMF
     with tempfile.NamedTemporaryFile() as domain_parts_out:
+        # TODO: Can it happen that there are multiple blocks? Do we need to stack something?
         meshio.write(domain_parts_out.name,
                      meshio.Mesh(points=msh.points,
                                  cells={cell_kind: msh.cells_dict[cell_kind]},
-                                 cell_data={cell_dataset: [msh.cell_data_dict["gmsh:physical"][cell_kind]]}),
+                                 cell_data={cell_dataset_name: [msh.cell_data_dict["gmsh:physical"][cell_kind]]}),
                      file_format="xdmf")
 
         mvc = dolfin.MeshValueCollection("size_t", mesh, mesh.topology().dim())
         with dolfin.XDMFFile(domain_parts_out.name) as domain_parts_in:
-            domain_parts_in.read(mvc, cell_dataset)
-        cf = dolfin.MeshFunction("size_t", mesh, mvc)
+            domain_parts_in.read(mvc, cell_dataset_name)
+        domain_parts = dolfin.MeshFunction("size_t", mesh, mvc)
 
-    # Write the final HDF5 file
-    with dolfin.HDF5File(mesh.mpi_comm(), str(dst), "w") as hdf:
-        hdf.write(mesh, f"/{mesh_dataset}")  # the mesh itself
-        hdf.write(cf, f"/{cell_dataset}")  # MeshFunction on cells of `mesh`
-        hdf.write(mf, f"/{boundary_dataset}")  # MeshFunction on facets of `fluid_mesh`
+    # Write the single HDF5 output file
+    write_hdf5_mesh(str(dst), mesh, domain_parts, boundary_parts)
 
 
 # # Some assumptions in this function, which is otherwise very nice, seem to be a little different from ours.
@@ -189,38 +247,3 @@ def import_gmsh(src: typing.Union[pathlib.Path, str],
 #     if prune_z:
 #         out_mesh.prune_z_0()
 #     return out_mesh
-
-
-def read_hdf5_mesh(filename: str) -> typing.Tuple[dolfin.Mesh, dolfin.MeshFunction, dolfin.MeshFunction]:
-    """Read an HDF5 mesh file created using `import_gmsh`.
-
-    The return value is `(mesh, domain_parts, boundary_parts)`.
-
-    Raises `ValueError` if the file does not have a "/mesh" dataset.
-
-    The "/domain_parts" and "/boundary_parts" datasets are optional.
-    If the file does not have them, the corresponding component of
-    the return value will be `None`.
-    """
-    with dolfin.HDF5File(dolfin.MPI.comm_world, filename, "r") as hdf:
-        if not hdf.has_dataset(mesh_dataset):
-            raise ValueError(f"{mesh_dataset} dataset not found in mesh file {filename}")
-
-        mesh = dolfin.Mesh()
-        hdf.read(mesh, mesh_dataset, False)  # target_object, data_path_in_hdf, use_existing_partitioning_if_any
-
-        if hdf.has_dataset(cell_dataset):
-            # For the tags, we must specify which mesh the MeshFunction belongs to,
-            # and the function's cell dimension.
-            domain_parts = dolfin.MeshFunction('size_t', mesh, mesh.topology().dim(), 0)  # type, mesh, dim, [default_value]
-            hdf.read(domain_parts, cell_dataset)
-        else:
-            domain_parts = None
-
-        if hdf.has_dataset(boundary_dataset):
-            boundary_parts = dolfin.MeshFunction('size_t', mesh, mesh.topology().dim() - 1, 0)
-            hdf.read(boundary_parts, boundary_dataset)
-        else:
-            boundary_parts = None
-
-    return mesh, domain_parts, boundary_parts
