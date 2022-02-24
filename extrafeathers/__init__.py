79ebe8e940190610b5c3b5761758d4c8d77364a2
Juha Jeronen <juha.m.jeronen@gmail.com>
2022-02-24 16:26:56 +0200

    update public API

--- extrafeathers/__init__.py
+++ extrafeathers/__init__.py
@@ -8,5 +8,7 @@ __version__ = "0.1.0"
 
 # export the public API
 from .autoboundary import *  # noqa: F401, F403
+from .common import *  # noqa: F401, F403
+from .meshfunction import *  # noqa: F401, F403
 from .meshiowrapper import *  # noqa: F401, F403
 from .plotutil import *  # noqa: F401, F403
