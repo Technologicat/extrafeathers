3c2d19652aefc82a66033c544bcf8a8e5301d452
Juha Jeronen <juha.m.jeronen@gmail.com>
2022-02-24 23:04:46 +0200

    use config

--- demo/coupled/main00_alternative_mesh.py
+++ demo/coupled/main00_alternative_mesh.py
@@ -10,15 +10,18 @@ from extrafeathers import meshfunction
 from extrafeathers import meshiowrapper
 from extrafeathers import plotmagic
 
+from .config import mesh_filename, Domains
+
+
 print(pathlib.Path.cwd())
 
 meshiowrapper.import_gmsh(src="demo/meshes/flow_over_two_cylinders.msh",
                           dst="demo/meshes/flow_over_cylinder_full.h5")  # for use by the flow solvers
 mesh, domain_parts, boundary_parts = meshiowrapper.read_hdf5_mesh("demo/meshes/flow_over_cylinder_full.h5")
-fluid_mesh = dolfin.SubMesh(mesh, domain_parts, 5)
+fluid_mesh = dolfin.SubMesh(mesh, domain_parts, Domains.FLUID.value)
 fluid_domain_parts = meshfunction.specialize(domain_parts, fluid_mesh)
 fluid_boundary_parts = meshfunction.specialize(boundary_parts, fluid_mesh)
-meshiowrapper.write_hdf5_mesh("demo/meshes/flow_over_cylinder_fluid.h5",
+meshiowrapper.write_hdf5_mesh(mesh_filename,
                               fluid_mesh, fluid_domain_parts, fluid_boundary_parts)
 
 plt.figure(1)
