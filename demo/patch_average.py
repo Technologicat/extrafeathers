be9f5c7b4a24aa7d99a1d1001c73345933f1ad75
Juha Jeronen <juha.m.jeronen@gmail.com>
2022-03-10 02:27:50 +0200

    show mesh in patch-averaging demo

--- demo/patch_average.py
+++ demo/patch_average.py
@@ -7,6 +7,8 @@ Works both serially and in MPI mode.
     mpirun python -m demo.patch_average
 """
 
+import numpy as np
+import matplotlib.tri as mtri
 import matplotlib.pyplot as plt
 
 import dolfin
@@ -18,15 +20,28 @@ V = dolfin.FunctionSpace(mesh, 'P', 1)
 f = dolfin.Expression('sin(N/4 * 2 * pi * x[0])', degree=2, N=N)
 g = dolfin.project(f, V)
 
+# TODO: need a utility for this. `demo.dofnumbering` needs something similar, too.
+def mpiplot_mesh():
+    cells, nodes_dict = extrafeathers.meshmagic.all_cells(V)
+    dofs, nodes = extrafeathers.meshmagic.nodes_to_array(nodes_dict)
+    if dolfin.MPI.comm_world.rank == 0:
+        cells = np.array(cells, dtype=np.int64)
+        # This relies on the fact that for p>1, in FEniCS the vertices are the first DOFs in each triangle.
+        tri = mtri.Triangulation(nodes[:, 0], nodes[:, 1], triangles=cells[:, :3])
+        plt.triplot(tri, color="#a0a0a0")
+
 if dolfin.MPI.comm_world.rank == 0:
     plt.subplot(2, 1, 1)
+    plt.title("Patch-averaging demo")
 theplot = extrafeathers.mpiplot(g)
+mpiplot_mesh()
 if dolfin.MPI.comm_world.rank == 0:
-    plt.title("Original function")
+    plt.ylabel("Original")
     plt.colorbar(theplot)
     plt.subplot(2, 1, 2)
 theplot = extrafeathers.mpiplot(extrafeathers.patch_average(g))
+mpiplot_mesh()
 if dolfin.MPI.comm_world.rank == 0:
-    plt.title("Patch average")
+    plt.ylabel("Patch-averaged")
     plt.colorbar(theplot)
     plt.show()
