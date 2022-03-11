a8f8204720af232310ae1a2e8b2772a9ac0d7aee
Juha Jeronen <juha.m.jeronen@gmail.com>
2022-03-12 01:51:46 +0200

    improve refelement demo

--- demo/refelement.py
+++ demo/refelement.py
@@ -9,11 +9,19 @@ Can be run serially or in parallel:
     mpirun -n 2 python -m demo.refelement
     mpirun python -m demo.refelement
 
-Line color is for MPI partitioning; node and text color is for cells.
+You can give an element type (Px or DPx) as argument:
+
+    python -m demo.refelement P2
+    python -m demo.refelement DP3
+
+Node and text color codes cells. Labels are "reference_dof (global_dof)".
+
+When running in parallel, line color codes MPI partitioning.
 """
 
 from collections import defaultdict
 from contextlib import contextmanager
+import sys
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -37,15 +45,22 @@ text_padding_left = 0.02  # in data coordinates
 colors = [item["color"] for item in mpl.rcParams["axes.prop_cycle"]]
 
 # Mix translucent versions. `colors` must be in "#rrggbb" format for this to work.
-def _colors_with_alpha(aa):
+def _colors_with_alpha(aa: str) -> list:
     return [f"{color}{aa}" for color in colors]
 colors80 = _colors_with_alpha("80")
 
 @contextmanager
-def mpl_largefont():
+def mpl_scalefont(factor: float = 2.0):
+    """Context manager: temporarily change Matplotlib font size.
+
+    Usage::
+
+        with mpl_scalefont(1.5):
+            plt.something(...)
+    """
     oldsize = mpl.rcParams["font.size"]
     try:
-        mpl.rcParams["font.size"] = oldsize * 2
+        mpl.rcParams["font.size"] = oldsize * factor
         yield
     finally:
         mpl.rcParams["font.size"] = oldsize
@@ -53,11 +68,12 @@ def mpl_largefont():
 # --------------------------------------------------------------------------------
 # Plot
 
-# TODO: parameterize element type (command-line argument, argparse)
+# Take element type from command-line argument if given
+arg = sys.argv[1] if len(sys.argv) > 1 else "P3"
+family, degree = arg[:-1], int(arg[-1])
+
 mesh = dolfin.UnitSquareMesh(2, 2)
-V = dolfin.FunctionSpace(mesh, "DP", 3)
-if dolfin.MPI.comm_world.rank == 0:
-    print(f"Total global #DOFS: {V.dim()}")
+V = dolfin.FunctionSpace(mesh, family, degree)
 
 dofmap = V.dofmap()
 element = V.element()
@@ -65,7 +81,8 @@ l2g = dofmap.tabulate_local_to_global_dofs()  # MPI-local to global
 
 if dolfin.MPI.comm_world.rank == 0:
     fig, ax = plt.subplots(1, 1, constrained_layout=True, figsize=(8, 8))
-plotmagic.mpiplot_mesh(V, linewidth=3.0, show_partitioning=True)
+show_partitioning = dolfin.MPI.comm_world.size > 1
+plotmagic.mpiplot_mesh(V, linewidth=3.0, show_partitioning=show_partitioning)
 
 data = []
 for cell in dolfin.cells(V.mesh()):
@@ -88,11 +105,12 @@ if dolfin.MPI.comm_world.rank == 0:
         label = f"{reference_dof} ({global_dof})"  # if not offset else f"[{reference_dof}]"
         plt.plot(node[0], node[1], colors80[cell_index % len(colors)],
                  marker="o", markersize=10.0)
-        text = plt.text(node[0] + offset[0] + text_padding_left,
-                        node[1] - offset[1], label,  # +y up so next line is in the -y direction
-                        color=colors[cell_index % len(colors)],
-                        horizontalalignment="left",
-                        verticalalignment="bottom")
+        with mpl_scalefont(1.1):
+            text = plt.text(node[0] + offset[0] + text_padding_left,
+                            node[1] - offset[1], label,  # +y up so next line is in the -y direction
+                            color=colors[cell_index % len(colors)],
+                            horizontalalignment="left",
+                            verticalalignment="bottom")
 
         # Increase offset by one line, for next element that shares this global DOF.
         # On getting the text display size in data coordinates in matplotlib, see:
@@ -101,12 +119,13 @@ if dolfin.MPI.comm_world.rank == 0:
         bbox_text = text.get_window_extent(renderer=renderer)
         bbox_text = Bbox(ax.transData.inverted().transform(bbox_text))  # to data coordinates
         new_y_offset = offset[1] + bbox_text.height
-        if new_y_offset > 2.9 * bbox_text.height:
-            offsets[key] = (offset[0] + bbox_text.width, 0.0)
-        else:
-            offsets[key] = (offset[0], offset[1] + bbox_text.height)
+        # if new_y_offset > 2.9 * bbox_text.height:  # three labels per column
+        #     offsets[key] = (offset[0] + bbox_text.width, 0.0)
+        # else:
+        #     offsets[key] = (offset[0], new_y_offset)
+        offsets[key] = (offset[0], new_y_offset)  # all labels in same column
 
-    mpi_str = f" (with {dolfin.MPI.comm_world.size} MPI processes)" if dolfin.MPI.comm_world.size > 1 else ""
-    plt.title(f"{V.ufl_element().family()} {V.ufl_element().degree()}{mpi_str}")
+    mpi_str = f"; {dolfin.MPI.comm_world.size} MPI processes" if dolfin.MPI.comm_world.size > 1 else ""
+    plt.title(f"{V.ufl_element().family()} {V.ufl_element().degree()}; {V.dim()} global DOFs on mesh{mpi_str}")
     plt.axis("off")
     plt.show()
