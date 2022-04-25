6b2b829e7111580e14c5d2f554bee3dcc787c6fa
Juha Jeronen <juha.m.jeronen@gmail.com>
2022-04-25 10:25:41 +0300

    add quad mesh

--- demo/euleriansolid/main00_alternative_mesh.py
+++ demo/euleriansolid/main00_alternative_mesh.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 from unpythonic import window
 
-from dolfin import UnitSquareMesh, MeshFunction, Facet, near, MPI
+import dolfin
 
 from extrafeathers import autoboundary
 from extrafeathers import meshiowrapper
@@ -132,24 +132,28 @@ def trimesh(nx, ny, align="x"):
 
 
 def main():
-    assert MPI.comm_world.size == 1, "Mesh can only be generated in serial mode, please run without mpirun."
+    assert dolfin.MPI.comm_world.size == 1, "Mesh can only be generated in serial mode, please run without mpirun."
 
     N = 16
 
     # https://fenicsproject.org/olddocs/dolfin/latest/cpp/de/dac/classdolfin_1_1UnitSquareMesh.html#a0cff260683cd6632fa569ddda6529a0d
     # 'std::string diagonal ("left", "right", "right/left", "left/right", or "crossed")
     #  indicates the direction of the diagonals.'
-    # mesh = UnitSquareMesh(N, N)
-    mesh = UnitSquareMesh(N, N, "crossed")
+    # mesh = dolfin.UnitSquareMesh(N, N)
+    # mesh = dolfin.UnitSquareMesh(N, N, "crossed")
     # mesh = trimesh(N, N)  # rows of equilateral triangles
     # mesh = trimesh(N, N, align="y")  # columns of equilateral triangles
+    mesh = dolfin.UnitSquareMesh.create(N, N, dolfin.CellType.Type.quadrilateral)
 
     from dolfin import ALE, Constant, FunctionSpace
     ALE.move(mesh, Constant((-0.5, -0.5)))
 
-    domain_parts = MeshFunction('size_t', mesh, mesh.topology().dim(), mesh.domains())
+    domain_parts = dolfin.MeshFunction('size_t', mesh, mesh.topology().dim(), mesh.domains())
 
-    V = FunctionSpace(mesh, "P", 1)
+    if mesh.cell_name() == "quadrilateral":
+        V = FunctionSpace(mesh, "Q", 1)
+    else:
+        V = FunctionSpace(mesh, "P", 1)
     ignored_cells, nodes_dict = meshmagic.all_cells(V)
     ignored_dofs, nodes_array = meshmagic.nodes_to_array(nodes_dict)
     xmin = np.min(nodes_array[:, 0])
@@ -159,32 +163,32 @@ def main():
 
     # `autoboundary` calls the callback for each facet on the external boundary
     # (i.e. the facet is on a boundary, and no neighboring subdomain exists).
-    def autoboundary_callback(submesh_facet: Facet, fullmesh_facet: Facet) -> typing.Optional[int]:
+    def autoboundary_callback(submesh_facet: dolfin.Facet, fullmesh_facet: dolfin.Facet) -> typing.Optional[int]:
         p = submesh_facet.midpoint()
         x, y = p.x(), p.y()
-        on_vert_boundary = near(y, ymin) or near(y, ymax)
-        on_horz_boundary = near(x, xmin) or near(x, xmax)
-        if near(x, xmin) and not on_vert_boundary:
+        on_vert_boundary = dolfin.near(y, ymin) or dolfin.near(y, ymax)
+        on_horz_boundary = dolfin.near(x, xmin) or dolfin.near(x, xmax)
+        if dolfin.near(x, xmin) and not on_vert_boundary:
             return Boundaries.LEFT.value
-        elif near(x, xmax) and not on_vert_boundary:
+        elif dolfin.near(x, xmax) and not on_vert_boundary:
             return Boundaries.RIGHT.value
-        elif near(y, ymax) and not on_horz_boundary:
+        elif dolfin.near(y, ymax) and not on_horz_boundary:
             return Boundaries.TOP.value
-        elif near(y, ymin) and not on_horz_boundary:
+        elif dolfin.near(y, ymin) and not on_horz_boundary:
             return Boundaries.BOTTOM.value
         return None  # this facet is not on a boundary we are interested in
 
     # Tag the boundaries.
-    boundary_parts: MeshFunction = autoboundary.find_subdomain_boundaries(fullmesh=mesh,
-                                                                          submesh=mesh,
-                                                                          subdomains=domain_parts,
-                                                                          boundary_spec={},
-                                                                          callback=autoboundary_callback)
+    boundary_parts: dolfin.MeshFunction = autoboundary.find_subdomain_boundaries(fullmesh=mesh,
+                                                                                 submesh=mesh,
+                                                                                 subdomains=domain_parts,
+                                                                                 boundary_spec={},
+                                                                                 callback=autoboundary_callback)
 
     meshiowrapper.write_hdf5_mesh(mesh_filename, mesh, None, boundary_parts)
 
-    from fenics import plot
-    plot(mesh)
+    plotmagic.mpiplot_mesh(V)
+    plt.axis("equal")
     plotmagic.plot_facet_meshfunction(boundary_parts, names=Boundaries)
     plt.legend(loc="best")
     plt.title("Generated mesh")
