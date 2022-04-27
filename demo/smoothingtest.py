0565ebd156e8fd8c798b7b3f7008fc1968b31db1
Juha Jeronen <juha.m.jeronen@gmail.com>
2022-04-27 14:51:35 +0300

    improve smoothing demo

--- demo/smoothingtest.py
+++ demo/smoothingtest.py
@@ -1,45 +1,109 @@
 # -*- coding: utf-8; -*-
 """Demonstrate the effects of Q1->Q0->Q1 projection smoothing."""
 
-import dolfin
+import sys
 
 import matplotlib.pyplot as plt
 
+import dolfin
+
+from extrafeathers import common
+from extrafeathers import meshmagic
 from extrafeathers import plotmagic
 
-N = 4
 
-mesh = dolfin.UnitSquareMesh.create(N, N, dolfin.CellType.Type.quadrilateral)
-V1 = dolfin.FunctionSpace(mesh, "Q", 1)
-V0 = dolfin.FunctionSpace(mesh, "DQ", 0)
+N = 8
+
+family = sys.argv[1] if len(sys.argv) > 1 else "Q"
+
+if family == "Q":
+    mesh = dolfin.UnitSquareMesh.create(N, N, dolfin.CellType.Type.quadrilateral)
+else:
+    mesh = dolfin.UnitSquareMesh(N, N, "crossed")
+
+V1 = dolfin.FunctionSpace(mesh, "P", 1)
+V0 = dolfin.FunctionSpace(mesh, "DP", 0)
 
 # x = dolfin.SpatialCoordinate(mesh)
 # ax = N * dolfin.pi * x[0]
 # ay = N * dolfin.pi * x[1]
 # fh = dolfin.project(dolfin.sin(ax) * dolfin.sin(ay), V1)
 
-f = dolfin.Expression("sin(N * pi * x[0]) * sin(N * pi * x[1])", N=N, degree=3)
-fh = dolfin.interpolate(f, V0)
+# Original function
+f = dolfin.Expression("x[0] * x[1]", degree=3)
+fh = dolfin.interpolate(f, V1)
 
-if dolfin.MPI.comm_world.rank == 0:
-    fig, ax = plt.subplots(1, 2, constrained_layout=True)
+# Stand-in for a checkerboard numerical oscillation we want to eliminate.
+# # Checkerboard function (when sampled on DQ0)
+# g = dolfin.Expression("1e6 * sin(N * pi * x[0]) * sin(N * pi * x[1])", N=N, degree=3)
+# gh = dolfin.interpolate(g, V0)
+# Checkerboard function at nodes of Q1
+g = dolfin.Expression("1e6 * cos(N * pi * x[0]) * cos(N * pi * x[1])", N=N, degree=3)
+gh = dolfin.interpolate(g, V1)
 
-if dolfin.MPI.comm_world.rank == 0:
-    plt.sca(ax[0])
-theplot = plotmagic.mpiplot(fh, show_mesh=True, show_partitioning=True, vmin=-1.0, vmax=1.0, cmap="Greys")
-if dolfin.MPI.comm_world.rank == 0:
-    plt.colorbar(theplot)
-    plt.axis("equal")
-    plt.title("Original DQ0 function")
+def doit(suptitle, error=None):
+    # Add simulated checkerboard oscillation
+    fh_distorted = dolfin.Function(fh.function_space())
+    if error:
+        fh_distorted.vector()[:] = fh.vector()[:] + error.vector()[:]
+    else:
+        fh_distorted.vector()[:] = fh.vector()[:]
 
-if dolfin.MPI.comm_world.rank == 0:
-    plt.sca(ax[1])
-fh = dolfin.project(fh, V1)
-theplot = plotmagic.mpiplot(fh, show_mesh=True, show_partitioning=True, vmin=-1.0, vmax=1.0, cmap="Greys")
-if dolfin.MPI.comm_world.rank == 0:
-    plt.colorbar(theplot)
-    plt.axis("equal")
-    plt.title("Projected to Q1")
+    # # DEBUG
+    # theplot = plotmagic.mpiplot(gh)
+    # plt.colorbar(theplot)
+    # plt.show()
+    # crash
+
+    if dolfin.MPI.comm_world.rank == 0:
+        fig, ax = plt.subplots(2, 2, constrained_layout=True, figsize=(8, 8))
+        plt.suptitle(suptitle)
+
+    def symmetric_vrange(p):
+        ignored_minp, maxp = common.minmax(p, take_abs=True, mode="raw")
+        return -maxp, maxp
+
+    if dolfin.MPI.comm_world.rank == 0:
+        plt.sca(ax[0, 0])
+    vmin, vmax = symmetric_vrange(fh_distorted)
+    theplot = plotmagic.mpiplot(fh_distorted, show_mesh=True, cmap="RdBu_r", vmin=vmin, vmax=vmax)
+    if dolfin.MPI.comm_world.rank == 0:
+        plt.colorbar(theplot)
+        plt.title("Input data")
+        plt.axis("equal")
+
+    if dolfin.MPI.comm_world.rank == 0:
+        plt.sca(ax[0, 1])
+    fdG0 = dolfin.project(fh_distorted, V0)
+    vmin, vmax = symmetric_vrange(fdG0)
+    theplot = plotmagic.mpiplot(fdG0, show_mesh=True, vmin=vmin, vmax=vmax, cmap="RdBu_r")
+    if dolfin.MPI.comm_world.rank == 0:
+        plt.colorbar(theplot)
+        plt.title("Projected to dG0")
+        plt.axis("equal")
+
+    if dolfin.MPI.comm_world.rank == 0:
+        plt.sca(ax[1, 0])
+    fP1 = dolfin.project(fdG0, V1)
+    vmin, vmax = symmetric_vrange(fP1)
+    theplot = plotmagic.mpiplot(fP1, show_mesh=True, vmin=vmin, vmax=vmax, cmap="RdBu_r")
+    if dolfin.MPI.comm_world.rank == 0:
+        plt.colorbar(theplot)
+        plt.title("Projected back")
+        plt.axis("equal")
+
+    if dolfin.MPI.comm_world.rank == 0:
+        plt.sca(ax[1, 1])
+    e = dolfin.project(fP1 - fh, V1)  # original fh, no error
+    vmin, vmax = symmetric_vrange(e)
+    theplot = plotmagic.mpiplot(e, show_mesh=True, vmin=vmin, vmax=vmax, cmap="RdBu_r")
+    if dolfin.MPI.comm_world.rank == 0:
+        plt.colorbar(theplot)
+        plt.title("Round-tripped minus clean input")
+        plt.axis("equal")
+
+doit("With clean original")
+doit("With checkerboard oscillation", error=gh)
 
 if dolfin.MPI.comm_world.rank == 0:
     plt.show()
