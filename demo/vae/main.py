8a7967274e52d43364b53eacc21d7f6ffa3040a9
Juha Jeronen <juha.m.jeronen@gmail.com>
2023-01-09 15:55:37 +0200

    VAE: use 100 epochs for better output (~8 min on Quadro RTX 3000)

--- demo/vae/main.py
+++ demo/vae/main.py
@@ -106,7 +106,7 @@ batch_size = 64  # acceptable generalization on continuous Bernoulli
 
 latent_dim = 2  # use a 2-dimensional latent space so that we can easily visualize the results
 
-epochs = 40
+epochs = 100
 
 # For a discussion of NN optimization methods, see the Deep Learning book by Goodfellow et al.
 optimizer = tf.keras.optimizers.Adam(1e-4)
