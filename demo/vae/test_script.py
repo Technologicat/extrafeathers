5403473a9da8a7e867bf9f16c941524a67b59142
Juha Jeronen <juha.m.jeronen@gmail.com>
2023-01-11 13:58:07 +0200

    update comment

--- demo/vae/test_script.py
+++ demo/vae/test_script.py
@@ -9,7 +9,7 @@ import demo.vae.main as main
 
 saved_model_dir = "demo/output/vae_test6/my_model"  # or whatever
 
-importlib.reload(main)  # so we can simply run this snippet again and it'll refresh the code
+importlib.reload(main)  # so we can simply run this snippet again to refresh the code (and reset the model)
 main.model.my_load(saved_model_dir)
 
 (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
