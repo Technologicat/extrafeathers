4a8b4d68f202076a77a84794e16eb5b9951060c5
Juha Jeronen <juha.m.jeronen@gmail.com>
2023-01-13 16:38:37 +0200

    mark a TODO

--- demo/vae/anim.py
+++ demo/vae/anim.py
@@ -11,6 +11,8 @@ run this script from the top-level `extrafeathers` directory as:
 !!! Run this script on the CPU; the GPU will easily run out of memory when plotting dataset evolution. !!!
 """
 
+# TODO: add an `ETAEstimator`, like in `main.py`
+
 import gc
 import glob
 import importlib
