a5eaa4cd980abb36f55060f3091b9d588f271e8c
Juha Jeronen <juha.m.jeronen@gmail.com>
2023-01-16 17:08:22 +0200

    VAE: improve docstrings/comments, fix ResNet convolution block

--- demo/vae/resnet.py
+++ demo/vae/resnet.py
@@ -1,10 +1,24 @@
-"""Residual network (ResNet) blocks for building slightly more advanced autoencoder architectures."""
+"""Residual network (ResNet) blocks, implemented as custom Keras layers.
 
-# We skip batch normalization for now because I'm not sure how to invert that in the decoder.
-# Based on combining information from:
-#   https://www.tensorflow.org/tutorials/customization/custom_layers
-#   https://www.analyticsvidhya.com/blog/2021/08/how-to-code-your-resnet-from-scratch-in-tensorflow/
-#   https://medium.com/swlh/how-to-create-a-residual-network-in-tensorflow-and-keras-cd97f6c62557
+These can be used e.g. for building slightly more advanced NN architectures for autoencoders.
+
+We skip batch normalization for now, because I'm new to the AI field, and not sure (as of
+this writing) how to invert a normalizer, which we would need to do to build a symmetric
+decoder.
+
+Also, BN won't do anything useful anyway until we set the `training` flag correctly, which
+`main.py` currently does not bother with. The network always thinks `training=False`,
+which would make BN produce nonsense, since it hasn't been calibrated.
+
+The implementation is based on combining information from:
+
+    https://www.tensorflow.org/tutorials/customization/custom_layers
+    https://www.analyticsvidhya.com/blog/2021/08/how-to-code-your-resnet-from-scratch-in-tensorflow/
+    https://medium.com/swlh/how-to-create-a-residual-network-in-tensorflow-and-keras-cd97f6c62557
+
+but mostly this is just a minimalistic jazz solo on the general idea of ResNets, for exploring them
+in the context of the MNIST datasets and `extrafeathers`.
+"""
 
 __all__ = ["IdentityBlock", "ConvolutionBlock",
            "IdentityBlockTranspose", "ConvolutionBlockTranspose"]
@@ -13,10 +27,23 @@ import tensorflow as tf
 
 # --------------------------------------------------------------------------------
 
+# TODO: rename -> IdentityBlock2D
 class IdentityBlock(tf.keras.layers.Layer):
-    """[batch, n, n, filters] -> [batch, n, n, filters]
+    """A simple ResNet identity block.
+
+    Tensor sizes::
+
+        [batch, n, n, filters] -> [batch, n, n, filters]
 
     The input must have `filters` channels so that the skip connection works.
+    To work with input with a different number of channels, use e.g. a convolution
+    layer with kernel size 1 as an adaptor::
+
+        # adaptor
+        x = Conv2D(filters=32, kernel_size=1, strides=1, padding="same", activation="relu")(x)
+
+        # now `x` can be fed into a 32-filter `IdentityBlock`
+        x = IdentityBlock(filters=32, kernel_size=3)(x)
     """
 
     def __init__(self, filters, kernel_size, *, name=None):
@@ -25,7 +52,8 @@ class IdentityBlock(tf.keras.layers.Layer):
                                             padding="same", activation="relu")
         self.conv2 = tf.keras.layers.Conv2D(filters=filters, kernel_size=kernel_size,
                                             padding="same", activation="relu")
-        # Activation of the last layer is handled in `call`.
+        # In the blocks defined in this module, activation of the last layer is handled in `call`
+        # because we need to add the residual from the skip-connection before applying the activation.
         self.conv3 = tf.keras.layers.Conv2D(filters=filters, kernel_size=1,
                                             padding="same")
         self.adder = tf.keras.layers.Add()
@@ -40,18 +68,27 @@ class IdentityBlock(tf.keras.layers.Layer):
         x = self.activation(x)
         return x
 
+# TODO: rename -> ConvolutionBlock2D
 class ConvolutionBlock(tf.keras.layers.Layer):
-    """[batch, 2*n, 2*n, channels] -> [batch, n, n, filters]"""
+    """A simple ResNet convolution block.
+
+    Tensor sizes::
+
+        [batch, 2*n, 2*n, filters // 2] -> [batch, n, n, filters]
+
+    The convention with `filters` is the same as in `Conv2D`; it's the number
+    of *output* channels.
+    """
 
     def __init__(self, filters, kernel_size, *, name=None):
         super().__init__(name=name)
-        self.conv1 = tf.keras.layers.Conv2D(filters=filters, kernel_size=1,
+        self.conv1 = tf.keras.layers.Conv2D(filters=filters // 2, kernel_size=1,
                                             padding="same", activation="relu")
         self.conv2 = tf.keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, strides=2,
                                             padding="same", activation="relu")
         self.conv3 = tf.keras.layers.Conv2D(filters=filters, kernel_size=1,
                                             padding="same")  # activation handled in `call`
-        # Classically, downsampling is done here by a convolution skipping 3/4 of the pixels:
+        # Classically, downsampling is done here by a size-1 convolution skipping 3/4 of the pixels:
         # self.downsample = tf.keras.layers.Conv2D(filters=filters, kernel_size=1, strides=2)
         # But perhaps we could try something like this:
         self.downsample = tf.keras.Sequential([tf.keras.layers.AveragePooling2D(pool_size=2, padding="same"),
@@ -71,8 +108,14 @@ class ConvolutionBlock(tf.keras.layers.Layer):
 
 # --------------------------------------------------------------------------------
 
+# TODO: rename -> IdentityBlockTranspose2D
 class IdentityBlockTranspose(tf.keras.layers.Layer):
-    """[batch, n, n, filters] -> [batch, n, n, filters]"""
+    """The architectural inverse of `IdentityBlock`, for autoencoder decoders.
+
+    Tensor sizes::
+
+        [batch, n, n, filters] -> [batch, n, n, filters]
+    """
 
     def __init__(self, filters, kernel_size, *, name=None):
         super().__init__(name=name)
@@ -94,14 +137,23 @@ class IdentityBlockTranspose(tf.keras.layers.Layer):
         x = self.activation(x)
         return x
 
+# TODO: rename -> ConvolutionBlockTranspose2D
 class ConvolutionBlockTranspose(tf.keras.layers.Layer):
-    """[batch, n, n, filters] -> [batch, 2*n, 2*n, filters]"""
+    """The architectural inverse of `ConvolutionBlock`, for autoencoder decoders.
+
+    Tensor sizes::
+
+        [batch, n, n, 2*filters] -> [batch, 2*n, 2*n, filters]
+
+    The convention with `filters` is the same as in `Conv2DTranspose`; it's the number
+    of *output* channels.
+    """
 
     def __init__(self, filters, kernel_size, *, name=None):
         super().__init__(name=name)
-        self.conv1 = tf.keras.layers.Conv2DTranspose(filters=filters, kernel_size=1,
+        self.conv1 = tf.keras.layers.Conv2DTranspose(filters=2 * filters, kernel_size=1,
                                                      padding="same", activation="relu")
-        self.conv2 = tf.keras.layers.Conv2DTranspose(filters=filters, kernel_size=kernel_size, strides=2,
+        self.conv2 = tf.keras.layers.Conv2DTranspose(filters=2 * filters, kernel_size=kernel_size, strides=2,
                                                      padding="same", activation="relu")
         self.conv3 = tf.keras.layers.Conv2DTranspose(filters=filters, kernel_size=1,
                                                      padding="same")  # activation handled in `call`
