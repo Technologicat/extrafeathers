1b6f3cf2c11b53e070231b69a2ccd2a6cfcf77d3
Juha Jeronen <juha.m.jeronen@gmail.com>
2023-05-10 14:31:20 +0300

    initial T profile: add piecewise solve for long simulations
    The time domain is split into segments 20 s in length each,
    solved sequentially; each piece gets `nel` elements, and
    duplicate points (from the seams) are automatically removed
    from the output.
    
    Also add an automatic cutoff once the difference to the external
    environment temperature falls under a threshold. When this occurs,
    the entire remainder of the simulation is then approximated by adding
    one data point at tmax (where the temperature has reached T_ext).

--- demo/euleriansolid/initial_T_profile.py
+++ demo/euleriansolid/initial_T_profile.py
@@ -7,7 +7,7 @@ https://fenicsproject.discourse.group/t/can-one-solve-system-of-ode-with-fenics/
 import numpy as np
 
 from dolfin import (IntervalMesh, interval,
-                    FiniteElement, FunctionSpace,
+                    FiniteElement, FunctionSpace, CompiledSubDomain,
                     DirichletBC, Constant, Expression, Function,
                     TestFunction, TrialFunction,
                     dx,
@@ -27,7 +27,11 @@ my_rank = MPI.comm_world.rank
 def estimate(tmax=20.0, nel=2000, degree=2):
     """Compute an educated guesstimate for the temperature profile at the inlet edge.
 
-    Automatically takes the material parameter config from `config.py`.
+    This automatically takes the material parameter config from `config.py`.
+    Solution method is finite elements in time, using classical nodal Lagrange elements.
+    If `tmax` is larger than a hardcoded limit, the solution proceeds piecewise.
+    Once `u - u_ext` falls below a hardcoded tolerance, the simulation stops,
+    and a final data point `(tmax, u_ext)` is appended to the result.
 
     We consider a small sphere of material, exposed to the environment, and integrate
     Newton's law of cooling. This gives us the temperature of the sphere as a function of time.
@@ -42,27 +46,22 @@ def estimate(tmax=20.0, nel=2000, degree=2):
     Parameters:
 
         `tmax`: End time for this 0D cooling simulation
-        `nel`: Number of elements. We use finite elements in time to perform the simulation.
-        `degree`: Element degree. We use classical nodal Lagrange elements.
+        `nel`: Number of elements. If solving piecewise, this is for each piece.
+               The default `nel` has been chosen to work well with the default `tmax`
+               when simulating 316L steel, starting from its solidus temperature
+               at 1700 K, and exposed to air at room temperature (22°C, 295.15 K).
+        `degree`: Degree of the Lagrange elements
 
-    Return value is `(u, xx, uu)`, where:
+    Return value is `(xx, uu)`, where:
 
-        `u`: The solution as a FEniCS `Function` (query it for `.function_space()` if needed)
         `xx`: rank-1 `np.array`, time coordinate of each node of the mesh
         `uu`: rank-1 `np.array`, solution value at the nodes
+
+    We return only NumPy arrays (no FEniCS function), because for large `tmax`, the
+    solution proceeds piecewise, so there is no global function for all times `t`.
     """
     if my_rank == 0:
         print(f"Solving inlet temperature profile with {nel} elements (degree {degree}), up to t = {tmax} s")
-
-    mesh = IntervalMesh(nel, 0, tmax)
-
-    Welm = FiniteElement("Lagrange", interval, degree)
-    W = FunctionSpace(mesh, Welm)
-
-    bcu = [DirichletBC(W, Constant(T0), "near(x[0], 0)")]
-
-    u = TrialFunction(W)
-    v = TestFunction(W)
 
     # How does a sphere of radius R cool, starting from initial temperature `u0`,
     # and exposed to an environment at temperature `u_ext`? Quick pen-and-paper
@@ -100,88 +99,153 @@ def estimate(tmax=20.0, nel=2000, degree=2):
     #
     #   ∫ u' v dx + (3 Γ / (R ρ)) ∫ (1 / c) [u - u_ext] v dx = 0
     #
-    c = Function(W, name="c")
-    weak_form = (u.dx(0) * v * dx +
-                 Constant(3 * Γ / (R * rho)) / c * (u - T_ext) * v * dx)
-    a = lhs(weak_form)
-    L = rhs(weak_form)
+    def solve_piece(t0, tmax, T0):
+        # Remap the interval to start from `t = 0`. This avoids triggering recompilation
+        # of the compiled subdomain for the BC, and thus runs much faster (especially
+        # when doing a piecewise solve).
+        tmax = tmax - t0
 
-    u_ = Function(W, name="u")
-    u_prev = Function(W, name="u_prev")
+        mesh = IntervalMesh(nel, 0.0, tmax)
 
-    # Nonlinear system, so we need an initial guess.
-    #
-    # We know the temperature `u` tends from `T0` to `T_ext` as t → ∞.
-    # We also know the behavior is of an exponentially saturating type,
-    # but we don't know the time constant (which, technically, isn't even a constant,
-    # because `c = c(u)`). So let's be very rough, and use a linearly decreasing
-    # temperature profile.
-    u_.assign(project(Expression("T_ext + (1.0 - (x[0] / tmax)) * (T0 - T_ext)",
-                                 degree=1,
-                                 tmax=tmax, T0=T0, T_ext=T_ext),
-                      W))
+        Welm = FiniteElement("Lagrange", interval, degree)
+        W = FunctionSpace(mesh, Welm)
 
-    n_system_iterations = 0
-    maxit = 20
-    tol = 1e-6
-    converged = False
-    while not converged:
-        n_system_iterations += 1
-        u_prev.assign(u_)
+        # t0_boundary = CompiledSubDomain(f"near(x[0], {t0})")
+        # bcu = [DirichletBC(W, Constant(T0), t0_boundary)]
+        # bcu = [DirichletBC(W, Constant(T0), f"near(x[0], {t0})")]
+        bcu = [DirichletBC(W, Constant(T0), "near(x[0], 0.0)")]
 
-        # update nonlinear coefficient field
-        c.assign(project(c_func(u_), W))
+        u = TrialFunction(W)
+        v = TestFunction(W)
 
-        # assemble and solve system
-        A = assemble(a)
-        b = assemble(L)
-        [bc.apply(A) for bc in bcu]
-        [bc.apply(b) for bc in bcu]
-        solve(A, u_.vector(), b, 'mumps')
-        # solve(A, u_.vector(), b, 'bicgstab', 'hypre_amg')  # more difficult to get to converge
+        c = Function(W, name="c")
+        weak_form = (u.dx(0) * v * dx +
+                     Constant(3 * Γ / (R * rho)) / c * (u - T_ext) * v * dx)
+        a = lhs(weak_form)
+        L = rhs(weak_form)
 
-        H1_diff = errornorm(u_, u_prev, "h1")
-        if H1_diff < tol:
-            converged = True
+        u_ = Function(W, name="u")
+        u_prev = Function(W, name="u_prev")
+
+        # Nonlinear system, so we need an initial guess.
+        #
+        # We know the temperature `u` tends from `T0` to `T_ext` as t → ∞.
+        # We also know the behavior is of an exponentially saturating type,
+        # but we don't know the time constant (which, technically, isn't even a constant,
+        # because `c = c(u)`). So let's be very rough, and use a linearly decreasing
+        # temperature profile.
+        u_.assign(project(Expression("T_ext + (1.0 - (x[0] / tmax)) * (T0 - T_ext)",
+                                     degree=1,
+                                     tmax=tmax, T0=T0, T_ext=T_ext),
+                          W))
+
+        n_system_iterations = 0
+        maxit = 20
+        tol = 1e-6
+        converged = False
+        while not converged:
+            n_system_iterations += 1
+            u_prev.assign(u_)
+
+            # update nonlinear coefficient field
+            c.assign(project(c_func(u_), W))
+
+            # assemble and solve system
+            A = assemble(a)
+            b = assemble(L)
+            [bc.apply(A) for bc in bcu]
+            [bc.apply(b) for bc in bcu]
+            solve(A, u_.vector(), b, 'mumps')
+            # solve(A, u_.vector(), b, 'bicgstab', 'hypre_amg')  # more difficult to get to converge
+
+            H1_diff = errornorm(u_, u_prev, "h1")
+            if H1_diff < tol:
+                converged = True
+            if my_rank == 0:
+                print(f"System iteration {n_system_iterations}, ‖u - u_prev‖_H1 = {H1_diff:0.6g}")
+            if n_system_iterations > maxit:
+                raise RuntimeError(f"System did not converge after {maxit} system iterations.")
+
         if my_rank == 0:
-            print(f"System iteration {n_system_iterations}, ‖u - u_prev‖_H1 = {H1_diff:0.6g}")
-        if n_system_iterations > maxit:
-            raise RuntimeError(f"System did not converge after {maxit} system iterations.")
+            print(f"System converged after {n_system_iterations} iterations.")
 
-    if my_rank == 0:
-        print(f"System converged after {n_system_iterations} iterations.")
+        # Extract the 1D array from the solution, using this approach:
+        # https://fenicsproject.discourse.group/t/convert-fenics-solution-and-coordinates-to-numpy-2d-arrays/3104
+        # x, = SpatialCoordinate(mesh)
 
-    # Extract the 1D array from the solution, using this approach:
-    # https://fenicsproject.discourse.group/t/convert-fenics-solution-and-coordinates-to-numpy-2d-arrays/3104
-    # x, = SpatialCoordinate(mesh)
+        # # works in serial mode only
+        # xx = W.tabulate_dof_coordinates()
+        # assert xx.shape[1] == 1
+        # xx = xx.flatten()
+        # uu = u_.vector().get_local()
 
-    # # works in serial mode only
-    # xx = W.tabulate_dof_coordinates()
-    # assert xx.shape[1] == 1
-    # xx = xx.flatten()
-    # uu = u_.vector().get_local()
+        # Let's use extrafeathers (works in MPI mode, too):
+        cells, nodes = meshmagic.all_cells(W)
+        dofs, nodes_array = meshmagic.nodes_to_array(nodes)
+        xx = nodes_array.flatten()
 
-    # Let's use extrafeathers (works in MPI mode, too):
-    cells, nodes = meshmagic.all_cells(W)
-    dofs, nodes_array = meshmagic.nodes_to_array(nodes)
-    xx = nodes_array.flatten()
+        all_W_dofs = np.array(range(W.dim()), "intc")
+        uu = Vector(MPI.comm_self)  # MPI-local, for receiving global DOF data on W
+        u_.vector().gather(uu, all_W_dofs)  # allgather
 
-    all_W_dofs = np.array(range(W.dim()), "intc")
-    uu = Vector(MPI.comm_self)  # MPI-local, for receiving global DOF data on W
-    u_.vector().gather(uu, all_W_dofs)  # allgather
+        perm = np.argsort(xx)  # sort the solution by increasing time coordinate
+        return t0 + xx[perm], uu[perm]
 
-    perm = np.argsort(xx)  # sort the solution by increasing time coordinate
-    return u_, xx[perm], uu[perm]
+    # Solve piecewise if necessary
+    tmax_limit = 20.0  # [s]
+    print("Solve for piece 0...")
+    xx, uu = solve_piece(t0=0.0, tmax=min(tmax, tmax_limit), T0=T0)
+    tol = 1e-8  # [K]
+    last_piece = False
+    if tmax > tmax_limit:
+        xxs, uus = [xx], [uu]
+        # Solve each piece
+        j = 1
+        while True:
+            print(f"Solve for piece {j}...")
+            piece_t0 = j * tmax_limit
+            piece_tmax = (j + 1) * tmax_limit
+
+            if piece_tmax >= tmax:
+                piece_tmax = tmax
+                last_piece = True
+
+            xx, uu = solve_piece(t0=piece_t0, tmax=piece_tmax, T0=uu[-1])
+            xxs.append(xx)
+            uus.append(uu)
+
+            if last_piece:
+                break
+            elif abs(uu[-1] - T_ext) <= tol:
+                print("Tolerance reached, setting the end temperature as T_ext.")
+                # Stop simulating once the temperature difference
+                # to the external environment falls below `tol`
+                xxs.append(np.array([tmax]))
+                uus.append(np.array([T_ext]))
+                break
+            j += 1
+        # Glue the pieces
+        xx, *xxs = xxs
+        uu, *uus = uus
+        for xx_piece, uu_piece in zip(xxs, uus):
+            if xx_piece[0] == xx[-1]:  # skip duplicated time value at the seams of pieces
+                xx_piece = xx_piece[1:]
+                uu_piece = uu_piece[1:]
+            xx = np.concatenate((xx, xx_piece))
+            uu = np.concatenate((uu, uu_piece))
+
+    return xx, uu
+
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
-    u_, xx, uu = estimate(tmax=20.0, nel=2000)
+    xx, uu = estimate(tmax=2000.0, nel=2000)
     if my_rank == 0:
         print(xx, uu)
 
         # visualize
-        plot(u_)
+        plt.plot(xx, uu)
         plt.grid(visible=True, which="both")
         plt.xlabel(r"$t$ [s]")
         plt.ylabel(r"$T$ [K]")
