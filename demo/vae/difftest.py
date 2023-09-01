d4edc6577335fa811a5eb2a17301ffca6e48fc10
Juha Jeronen <juha.m.jeronen@gmail.com>
2023-09-01 09:08:25 +0300

    refactor differentiation

--- demo/vae/difftest.py
+++ demo/vae/difftest.py
@@ -18,6 +18,26 @@ import tensorflow as tf
 
 import matplotlib.pyplot as plt
 
+
+def make_stencil(N: int) -> np.array:
+    """Return an array of integer offset pairs for a stencil of N×N points.
+
+    `N`: neighborhood size parameter
+
+    Return value is a rank-2 np-array of shape [n_neighbors, 2].
+    """
+    neighbors = [[iy, ix] for iy in range(-N, N + 1)
+                          for ix in range(-N, N + 1)
+                          if not (iy == 0 and ix == 0)]
+    neighbors = np.array(neighbors, dtype=int)
+    return neighbors
+
+
+# The edges are nonsense with padding="SAME", so we use "VALID", and chop off the edges of X and Y correspondingly.
+def chop_edges(N: int, X, Y):
+    return X[N:-N, N:-N], Y[N:-N, N:-N]
+
+
 # TODO: this is now `padding="valid"`; implement `padding="same"` mode (extrapolate linearly?)
 def denoise(N: int, f):
     """Attempt to denoise the function values data.
@@ -30,26 +50,95 @@ def denoise(N: int, f):
 
     but we use a simple discrete implementation, and only as a preprocessor.
 
-    `N`: neighborhood size, for constructing the box to look up neighbors in.
+    `N`: neighborhood size parameter
     """
     def friedrichs_mollifier(x, *, eps=0.001):  # not normalized!
         return np.where(np.abs(x) < 1 - eps, np.exp(-1 / (1 - x**2)), 0.)
 
-    offset_X, offset_Y = np.meshgrid(np.arange(-N, N + 1), np.arange(-N, N + 1))  # neighbor offsets
-    offset_R = np.sqrt(offset_X**2 + offset_Y**2)
-    rmax = np.ceil(np.max(offset_R))  # the offset distance at which we want the mollifier to vanish
+    offset_X, offset_Y = np.meshgrid(np.arange(-N, N + 1), np.arange(-N, N + 1))  # neighbor offsets (in grid points)
+    offset_R = np.sqrt(offset_X**2 + offset_Y**2)  # euclidean
+    rmax = np.ceil(np.max(offset_R))  # the grid distance at which we want the mollifier to become zero
 
-    smoothing_kernel = friedrichs_mollifier(offset_R / rmax)
-    smoothing_kernel = smoothing_kernel / np.sum(smoothing_kernel)
+    kernel = friedrichs_mollifier(offset_R / rmax)
+    kernel = kernel / np.sum(kernel)
 
     f = tf.expand_dims(f, axis=0)  # batch
     f = tf.expand_dims(f, axis=-1)  # channels
-    smoothing_kernel = tf.expand_dims(smoothing_kernel, axis=-1)  # input channels
-    smoothing_kernel = tf.expand_dims(smoothing_kernel, axis=-1)  # output channels
-    f = tf.nn.convolution(f, smoothing_kernel, padding="VALID")
+    kernel = tf.expand_dims(kernel, axis=-1)  # input channels
+    kernel = tf.expand_dims(kernel, axis=-1)  # output channels
+    f = tf.nn.convolution(f, kernel, padding="VALID")
     f = tf.squeeze(f, axis=-1)  # channels
     f = tf.squeeze(f, axis=0)  # batch
     return f.numpy()
+
+
+# TODO: this is now `padding="valid"`; implement `padding="same"` mode (extrapolate linearly?)
+coeffs = {"dx": 0, "dy": 1, "dx2": 2, "dxdy": 3, "dy2": 4}
+def differentiate(N, X, Y, Z):
+    """Fit a 2nd order surrogate polynomial to data values on a meshgrid, to estimate derivatives.
+
+    Note the distance matrix `A` (generated automatically) is 5×5 regardless of `N`, but for large `N`,
+    assembly takes longer because there are more contributions to each matrix element.
+
+    Note we lose the edges, like in a convolution with padding="VALID", essentially for the same reason.
+    This is mathematically trivial to fix (we should just build `A` and `b` without the missing neighbors,
+    or take more neighbors from the existing side - even the sizes of A and b do not change), but the code
+    becomes unwieldy for a simple example so we haven't done that.
+
+    `N`: neighborhood size parameter
+    """
+    neighbors = make_stencil(N)
+
+    # Derivative scaling for numerical stability: x' := x / xscale  ⇒  d/dx → (1 / xscale) d/dx'.
+    # Choose xscale so that the magnitudes are near 1. Similarly for y. We use the grid spacing as the scale.
+    xscale = X[0, 1] - X[0, 0]
+    yscale = Y[1, 0] - Y[0, 0]
+    def fcoeffs(dx, dy):
+        dx = dx / xscale
+        dy = dy / yscale
+        return np.array([dx, dy, 0.5 * dx**2, dx * dy, 0.5 * dy**2])  # Taylor
+    ncoeffs = len(fcoeffs(0, 0))
+
+    # Since we have a uniform grid in this application, the distance matrix of neighbors for each point is the same,
+    # so we need to assemble only one.
+    # TODO: tensorize? (we might have a lot of neighbors)
+    A = np.zeros((ncoeffs, ncoeffs))
+    iy, ix = N, N  # Any node in the interior is fine, since the local topology and geometry are the same for all of them.
+    for offset_y, offset_x in neighbors:
+        dx = X[iy + offset_y, ix + offset_x] - X[iy, ix]
+        dy = Y[iy + offset_y, ix + offset_x] - Y[iy, ix]
+        c = fcoeffs(dx, dy)
+        for j in range(ncoeffs):
+            for n in range(ncoeffs):
+                A[j, n] += c[j] * c[n]
+    A = tf.constant(A)
+
+    # Form the right-hand side for each point. This is the only part that depends on the data values f.
+    # TODO: tensorize, this is the slowest part, and O(n) on the data.
+    ny, nx = np.shape(X)
+    bs = []
+    for iy in range(N, ny - N):
+        for ix in range(N, nx - N):
+            b = np.zeros((ncoeffs,))
+            for offset_y, offset_x in neighbors:
+                dx = X[iy + offset_y, ix + offset_x] - X[iy, ix]
+                dy = Y[iy + offset_y, ix + offset_x] - Y[iy, ix]
+                c = fcoeffs(dx, dy)
+                for j in range(ncoeffs):
+                    b[j] += (Z[iy + offset_y, ix + offset_x] - Z[iy, ix]) * c[j]
+            bs.append(b)
+    bs = tf.stack(bs, axis=1)
+
+    # The solution of the linear systems (one per data point) yields the jacobian and hessian of the surrogate.
+    df = tf.linalg.solve(A, bs)  # [ncoeffs, n_datapoints]
+
+    # Undo the derivative scaling,  d/dx' → d/dx
+    scale = tf.constant([xscale, yscale, xscale**2, xscale * yscale, yscale**2])
+    scale = tf.expand_dims(scale, axis=-1)  # for broadcasting to all data points
+    df = df / scale
+
+    df = tf.reshape(df, (ncoeffs, ny - 2 * N, nx - 2 * N))
+    return df
 
 
 def main():
@@ -87,117 +176,21 @@ def main():
     X, Y = np.meshgrid(xx, yy)
     f = f(X, Y)
 
-    neighbors = [[iy, ix] for iy in range(-N, N + 1)
-                          for ix in range(-N, N + 1)
-                          if not (iy == 0 and ix == 0)]
-    neighbors = np.array(neighbors, dtype=int)
-    print(f"Using {len(neighbors)}-point neighborhoods (N = {N}).")
-
     # --------------------------------------------------------------------------------
     # Noisy input simulation.
     if σ > 0:
-        # Corrupt the data with synthetic noise
+        # Corrupt the data with synthetic noise...
         noise = np.random.normal(loc=0.0, scale=σ, size=np.shape(X))
         f += noise
 
+        # ...and then attempt to remove the noise.
         f = denoise(N, f)
-
-        # The edges are nonsense with padding="SAME", so we use "VALID", and chop off the edges of X and Y correspondingly.
-        X = X[N:-N, N:-N]
-        Y = Y[N:-N, N:-N]
+        X, Y = chop_edges(N, X, Y)
 
     # --------------------------------------------------------------------------------
-    # # Fit a 2nd order surrogate polynomial to obtain derivatives.
-    # # Old inefficient code.
-    # TODO: how to vectorize this?
-    # TODO: how to do this in TF? (EagerTensor does not support item assignment)
-    # ny, nx = np.shape(X)
-    # fcoeffs = lambda dx, dy: [dx, dy, 0.5 * dx**2, dx * dy, 0.5 * dy**2]  # Taylor
-    # ncoeffs = len(fcoeffs(0, 0))
-    # coeff = {"dx": 0, "dy": 1, "dx2": 2, "dxdy": 3, "dy2": 4}
-    # As = []
-    # bs = []
-    # for iy in range(N, ny - N):
-    #     for ix in range(N, nx - N):
-    #         A = np.zeros((ncoeffs, ncoeffs))
-    #         b = np.zeros((ncoeffs,))
-    #         for offset_y, offset_x in neighbors:
-    #             dx = X[iy + offset_y, ix + offset_x] - X[iy, ix]
-    #             dy = Y[iy + offset_y, ix + offset_x] - Y[iy, ix]
-    #             c = fcoeffs(dx, dy)
-    #             for j in range(ncoeffs):
-    #                 for n in range(ncoeffs):
-    #                     A[j, n] += c[j] * c[n]
-    #                 b[j] += (f[iy + offset_y, ix + offset_x] - f[iy, ix]) * c[j]
-    #         As.append(A)
-    #         bs.append(b)
-    # As = tf.stack(As, axis=0)
-    # bs = tf.stack(bs, axis=0)
-    # bs = tf.expand_dims(bs, axis=-1)
-    # df = tf.linalg.solve(As, bs)
-    # df = tf.squeeze(df, axis=-1)
-    # df = tf.reshape(df, (ny - 2 * N, nx - 2 * N, ncoeffs))  # [iy, ix, c]
+    # Compute the derivatives.
 
-    # --------------------------------------------------------------------------------
-    # Fit a 2nd order surrogate polynomial to obtain derivatives.
-
-    # Since we have a uniform grid in this application, the distance matrix of neighbors for each point is the same,
-    # so we need to assemble only one.
-    #
-    # Note A is 5×5 regardless of N, but for large N, assembly takes longer because there are more contributions
-    # to each matrix element.
-    #
-    # Note we lose the edges, like in a convolution with padding="VALID", essentially for the same reason.
-    # Here this is mathematically trivial to fix (just build A and b without the missing neighbors, or take
-    # more neighbors from the existing side - even the sizes of A and b do not change), but the code becomes
-    # unwieldy for a simple example.
-    #
-    # fcoeffs = lambda dx, dy: [dx, dy, 0.5 * dx**2, dx * dy, 0.5 * dy**2]  # Taylor
-
-    # Derivative scaling for numerical stability: x' := x / xscale  ⇒  d/dx → (1 / xscale) d/dx'.
-    # Choose xscale so that the magnitudes are near 1. Similarly for y. We use the grid spacing as the scale.
-    xscale = X[0, 1] - X[0, 0]
-    yscale = Y[1, 0] - Y[0, 0]
-    def fcoeffs(dx, dy):
-        dx = dx / xscale
-        dy = dy / yscale
-        return [dx, dy, 0.5 * dx**2, dx * dy, 0.5 * dy**2]  # Taylor
-
-    ncoeffs = len(fcoeffs(0, 0))
-    coeff = {"dx": 0, "dy": 1, "dx2": 2, "dxdy": 3, "dy2": 4}
-    A = np.zeros((ncoeffs, ncoeffs))
-    iy, ix = N, N  # Any node in the interior is fine, since the local topology and geometry are the same for all of them.
-    for offset_y, offset_x in neighbors:
-        dx = X[iy + offset_y, ix + offset_x] - X[iy, ix]
-        dy = Y[iy + offset_y, ix + offset_x] - Y[iy, ix]
-        c = fcoeffs(dx, dy)
-        for j in range(ncoeffs):
-            for n in range(ncoeffs):
-                A[j, n] += c[j] * c[n]
-    # Form the right-hand side for each point. This is the only part that depends on the data values f.
-    ny, nx = np.shape(X)
-    bs = []
-    for iy in range(N, ny - N):
-        for ix in range(N, nx - N):
-            b = np.zeros((ncoeffs,))
-            for offset_y, offset_x in neighbors:
-                dx = X[iy + offset_y, ix + offset_x] - X[iy, ix]
-                dy = Y[iy + offset_y, ix + offset_x] - Y[iy, ix]
-                c = fcoeffs(dx, dy)
-                for j in range(ncoeffs):
-                    b[j] += (f[iy + offset_y, ix + offset_x] - f[iy, ix]) * c[j]
-            bs.append(b)
-    bs = tf.stack(bs, axis=1)
-
-    # The solution of the linear systems (one per data point) yields the jacobian and hessian of the surrogate.
-    df = tf.linalg.solve(A, bs)  # [ncoeffs, n_datapoints]
-
-    # Undo the derivative scaling,  d/dx' → d/dx
-    scale = tf.constant([xscale, yscale, xscale**2, xscale * yscale, yscale**2])
-    scale = tf.expand_dims(scale, axis=-1)  # for broadcasting to all data points
-    df = df / scale
-
-    df = tf.reshape(df, (ncoeffs, ny - 2 * N, nx - 2 * N))
+    df = differentiate(N, X, Y, f)
 
     # --------------------------------------------------------------------------------
     # Plot the results
@@ -213,9 +206,9 @@ def main():
     ax.set_zlabel("z")
     ax.set_title("f")
     all_axes = [ax]
-    for idx, key in enumerate(coeff.keys(), start=2):
+    for idx, key in enumerate(coeffs.keys(), start=2):
         ax = fig.add_subplot(2, 3, idx, projection="3d")
-        surf = ax.plot_surface(X[N:-N, N:-N], Y[N:-N, N:-N], df[coeff[key], :, :], linewidth=0, antialiased=False)  # noqa: F841
+        surf = ax.plot_surface(X[N:-N, N:-N], Y[N:-N, N:-N], df[coeffs[key], :, :], linewidth=0, antialiased=False)  # noqa: F841
         ax.set_xlabel("x")
         ax.set_ylabel("y")
         ax.set_zlabel("z")
@@ -223,7 +216,7 @@ def main():
         all_axes.append(ax)
 
         ground_truth = ground_truths[key](X[N:-N, N:-N], Y[N:-N, N:-N])
-        max_l1_error = np.max(np.abs(ground_truth - df[coeff[key], :, :]))
+        max_l1_error = np.max(np.abs(ground_truth - df[coeffs[key], :, :]))
         print(f"max absolute l1 error {key} = {max_l1_error:0.3g}")
     fig.suptitle(f"Local quadratic surrogate fit, noise σ = {σ:0.3g}")
 
