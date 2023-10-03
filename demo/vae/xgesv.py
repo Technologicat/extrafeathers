554a0c2ae141d7ae390ace4cdda4a83e2a612be6
Juha Jeronen <juha.m.jeronen@gmail.com>
2023-10-03 16:59:05 +0300

    xgesv: preliminary batch mode support

--- demo/vae/xgesv.py
+++ demo/vae/xgesv.py
@@ -1,17 +1,19 @@
-"""A simple partial pivoting (row swap) solver.
+"""A simple partial pivoting (row swap) linear equation system solver.
 
-This is a TensorFlow port of the basic functionality in:
+This is a TensorFlow port of the basic functionality of:
   https://github.com/Technologicat/pylu/blob/master/pylu/dgesv.pxd
 
 The point is to allow any dtype in some simple use cases; as of TF 2.12,
 `tf.linalg.solve` does not have a kernel for float16.
 
 This solver, instead, is based on high-level TensorFlow operations,
-so that it can be run with any supported dtype.
+so it can be run with any supported dtype.
 """
 
-__all__ = ["decompose",
-           "solve"]
+__all__ = ["decompose",  # batched, many independent equation systems
+           "solve",  # batched, many independent equation systems
+           "decompose_one",  # one equation system
+           "solve_one"]  # one equation system
 
 import typing
 
@@ -33,51 +35,65 @@ def decompose(a: tf.Tensor) -> typing.Tuple[tf.Tensor, tf.Tensor]:
 
        `p`: rank-2 tensor, [batch, n]; permutation vectors used in partial pivoting.
     """
-    n = int(tf.shape(a)[-1])
+    shape = tf.shape(a)
+    if len(shape) != 3 or shape[1] != shape[2]:
+        raise ValueError(f"Expected `a` to be a tensor of shape [batch, n, n], got {shape}")
+    batch = int(shape[0])
+    n = int(shape[1])
 
-    # TODO: batch support
-
-    # TODO: data placement woes; this doesn't help: with tf.device('GPU:0'):
+    # TODO: fix data placement woes with `jit_compile=True`; this doesn't help: with tf.device('GPU:0'):
 
     lu = tf.Variable(a, name="lu", trainable=False)  # copy `a` into a variable `LU`, which we will decompose in-place
-    p = tf.Variable(tf.range(n), name="p", trainable=False)
-    decompose_kernel(lu, p)
+    p = tf.range(n)
+    p = tf.expand_dims(p, axis=0)  # [1, n]
+    p = tf.tile(p, [batch, 1])  # [batch, n]
+    p = tf.Variable(p, name="p", trainable=False)
+
+    wrkf = tf.Variable(tf.zeros([batch], dtype=a.dtype), name="wrkf")
+    wrki = tf.Variable(tf.zeros([batch], dtype=tf.int32), name="wrki")
+
+    decompose_kernel(lu, p, wrkf, wrki)
 
     return lu, p
 
 
+# wrkf and wrki are float and integer work space vectors, respectively.
 @tf.function
-def decompose_kernel(lu: tf.Variable, p: tf.Variable) -> None:
+def decompose_kernel(lu: tf.Variable, p: tf.Variable,
+                     wrkf: tf.Variable, wrki: tf.Variable) -> None:
     """The actual compute kernel. Modifies `lu` in-place."""
-    n = int(tf.shape(lu)[-1])
-
-    # TODO: batch support
+    shape = tf.shape(lu)
+    batch = int(shape[0])
+    n = int(shape[1])
 
     for k in range(n):
         # Pivot: find row `r` such that `|a_rk| = max( |a_sk|, s = k, k+1, ..., n-1 )` (note 0-based indexing)
-        max_mag = tf.cast(-1.0, lu.dtype)  # any invalid value
-        r = -1
+        wrkf[:].assign(-1.0)  # max magnitude, for each `lu` in batch; any invalid value
+        wrki[:].assign(-1)  # row `r`, for each `lu` in batch
         for s in range(k, n):
-            mag = tf.abs(lu[s, k])
-            if mag > max_mag:
-                max_mag = mag
-                r = s
+            mag = tf.abs(lu[:, s, k])  # [batch]
+            wrki.assign(tf.where(tf.greater(mag, wrkf), s, wrki))  # if mag > max_mag then s, else old r
+            wrkf.assign(tf.where(tf.greater(mag, wrkf), mag, wrkf))  # update max_mag
 
         # Swap elements `k` and `r` of permutation vector `p`
-        tmp1 = p[k]
-        p[k].assign(p[r])
-        p[r].assign(tmp1)
+        tmp1 = p[:, k]
+        p[:, k].assign(tf.gather(p, wrki, axis=1))
+        for m in range(batch):  # TODO: tensorize over batch
+            # p[m, k].assign(p[m, wrki[m]])
+            p[m, wrki[m]].assign(tmp1[m])
 
         # Physically swap also the corresponding rows of A.
         #
         # This may seem a silly way to do it (why not indirect via p?), but it makes future accesses faster,
         # because then we won't need to use the permutation vector p when accessing A or lu.
         #
-        # (It is still needed for accessing the load vector when solving the actual equation system.)
+        # (It is still needed for accessing the RHS vector when solving the actual equation system.)
         #
-        tmp2 = lu[k, :]
-        lu[k, :].assign(lu[r, :])
-        lu[r, :].assign(tmp2)
+        tmp2 = lu[:, k, :]
+        lu[:, k, :].assign(tf.gather(lu, wrki, axis=1))
+        for m in range(batch):  # TODO: tensorize over batch
+            # lu[m, k, :].assign(lu[m, wrki[m], :])
+            lu[m, wrki[m], :].assign(tmp2[m, :])
 
         # # TODO: Fail gracefully if the pivoted lu[k, k] is below some tolerance
         # # TODO: make the tolerance depend on dtype
@@ -85,9 +101,9 @@ def decompose_kernel(lu: tf.Variable, p: tf.Variable) -> None:
         #     return False
 
         for i in range(k + 1, n):
-            lu[i, k].assign(lu[i, k] / lu[k, k])
+            lu[:, i, k].assign(lu[:, i, k] / lu[:, k, k])
             for j in range(k + 1, n):
-                lu[i, j].assign(lu[i, j] - lu[i, k] * lu[k, j])
+                lu[:, i, j].assign(lu[:, i, j] - lu[:, i, k] * lu[:, k, j])
 
 
 def solve(lu: tf.Tensor, p: tf.Tensor, b: tf.Tensor) -> tf.Tensor:
@@ -118,11 +134,18 @@ def solve(lu: tf.Tensor, p: tf.Tensor, b: tf.Tensor) -> tf.Tensor:
 
     Returns `x`, a rank-2 tensor [batch, n]; solution for `P A x = P b`.
     """
-    n = int(tf.shape(lu)[-1])
+    shape = tf.shape(lu)
+    if len(shape) != 3 or shape[1] != shape[2]:
+        raise ValueError(f"Expected `a` to be a tensor of shape [batch, n, n], got {shape}")
+    batch = int(shape[0])
+    n = int(shape[1])
 
-    # TODO: batch support
+    for name, tensor in (("p", p), ("b", b)):
+        shape = tf.shape(tensor)
+        if len(shape) != 2 or shape[1] != n:
+            raise ValueError(f"Expected {name} to be a tensor of shape [batch, n], and from `lu`, n = {n}; got {shape}")
 
-    x = tf.Variable(tf.zeros([n], dtype=lu.dtype), name="x", trainable=False)
+    x = tf.Variable(tf.zeros([batch, n], dtype=lu.dtype), name="x", trainable=False)
     solve_kernel(lu, p, b, x)
     return x
 
@@ -130,9 +153,138 @@ def solve(lu: tf.Tensor, p: tf.Tensor, b: tf.Tensor) -> tf.Tensor:
 @tf.function
 def solve_kernel(lu: tf.Tensor, p: tf.Tensor, b: tf.Tensor, x: tf.Variable) -> None:
     """The actual compute kernel. Writes `x` in-place."""
+    shape = tf.shape(lu)
+    # batch = int(shape[0])
+    n = int(shape[1])
+
+    # Solve `L y = P b` by forward substitution.
+    for i in range(n):
+        x[:, i].assign(tf.gather(b, p[:, i], axis=1))  # formally, `(P b)[i]`
+        for j in range(i):
+            x[:, i].assign(x[:, i] - lu[:, i, j] * x[:, j])
+    # Now `x` contains the solution of the first equation, i.e. it is actually `y`.
+
+    # Solve `U x = y` by backward substitution.
+    for i in range(n - 1, -1, -1):
+        for j in range(i + 1, n):
+            x[:, i].assign(x[:, i] - lu[:, i, j] * x[:, j])
+        x[:, i].assign(x[:, i] / lu[:, i, i])
+    # Now the array "x" contains the solution of the second equation, i.e. x.
+
+
+# --------------------------------------------------------------------------------
+# For one equation system.
+#
+# These are here mainly because the code is much more readable than the batched version,
+# and more readily comparable to `dgesv.pxd` in PyLU.
+
+def decompose_one(a: tf.Tensor) -> typing.Tuple[tf.Tensor, tf.Tensor]:
+    """LU decompose an `n × n` matrix, using partial pivoting (row swaps).
+
+    Produces matrices `L` and `U` such that `P A = L U`, where `P` is a row-swapping permutation matrix.
+
+    `A`: rank-2 tensor, [n, n], C storage order. A square matrix to be LU decomposed.
+
+    Returns: `(LU, p)`, where
+       `LU`: rank-2 tensor, [n, n]; the `L` and `U` matrices.
+
+             The storage format is packed so that the diagonal elements of `L` are implicitly 1 (not stored).
+             The diagonal of `LU` stores the diagonal elements of `U`.
+
+       `p`: rank-1 tensor, [n]; permutation vector used in partial pivoting.
+    """
+    n = int(tf.shape(a)[-1])
+
+    # TODO: data placement woes; this doesn't help: with tf.device('GPU:0'):
+
+    lu = tf.Variable(a, name="lu", trainable=False)  # copy `a` into a variable `LU`, which we will decompose in-place
+    p = tf.Variable(tf.range(n), name="p", trainable=False)
+    decompose_one_kernel(lu, p)
+
+    return lu, p
+
+
+@tf.function
+def decompose_one_kernel(lu: tf.Variable, p: tf.Variable) -> None:
+    """The actual compute kernel. Modifies `lu` in-place."""
     n = int(tf.shape(lu)[-1])
 
-    # TODO: batch support
+    for k in range(n):
+        # Pivot: find row `r` such that `|a_rk| = max( |a_sk|, s = k, k+1, ..., n-1 )` (note 0-based indexing)
+        max_mag = tf.cast(-1.0, lu.dtype)  # any invalid value
+        r = -1
+        for s in range(k, n):
+            mag = tf.abs(lu[s, k])
+            if mag > max_mag:
+                max_mag = mag
+                r = s
+
+        # Swap elements `k` and `r` of permutation vector `p`
+        tmp1 = p[k]
+        p[k].assign(p[r])
+        p[r].assign(tmp1)
+
+        # Physically swap also the corresponding rows of A.
+        #
+        # This may seem a silly way to do it (why not indirect via p?), but it makes future accesses faster,
+        # because then we won't need to use the permutation vector p when accessing A or lu.
+        #
+        # (It is still needed for accessing the RHS vector when solving the actual equation system.)
+        #
+        tmp2 = lu[k, :]
+        lu[k, :].assign(lu[r, :])
+        lu[r, :].assign(tmp2)
+
+        # # TODO: Fail gracefully if the pivoted lu[k, k] is below some tolerance
+        # # TODO: make the tolerance depend on dtype
+        # if lu[k, k] <= 1e-6:
+        #     return False
+
+        for i in range(k + 1, n):
+            lu[i, k].assign(lu[i, k] / lu[k, k])
+            for j in range(k + 1, n):
+                lu[i, j].assign(lu[i, j] - lu[i, k] * lu[k, j])
+
+
+def solve_one(lu: tf.Tensor, p: tf.Tensor, b: tf.Tensor) -> tf.Tensor:
+    """Solve a linear equation system after the matrix has been LU-decomposed.
+
+    `lu`: rank-2 tensor, [n, n], C storage order. Packed LU decomposition from `decompose`.
+    `p`: rank-1 tensor, [n]. Permutation vector from `decompose`.
+    `b`: rank-1 tensor, [n]. Right-hand side of `A x = b`.
+
+    The equation to be solved is::
+
+      A x = b
+
+    in the form::
+
+      P A x = P b
+
+    where P is a permutation matrix (that permutes rows), and `P A = L U`. Hence::
+
+      L U x = P b
+
+    This equation is equivalent with the system::
+
+      L y = P b
+      U x = y
+
+    We first solve `L y = P b` and then `U x = y`.
+
+    Returns `x`, a rank-1 tensor [n]; solution for `P A x = P b`.
+    """
+    n = int(tf.shape(lu)[-1])
+
+    x = tf.Variable(tf.zeros([n], dtype=lu.dtype), name="x", trainable=False)
+    solve_one_kernel(lu, p, b, x)
+    return x
+
+
+@tf.function
+def solve_one_kernel(lu: tf.Tensor, p: tf.Tensor, b: tf.Tensor, x: tf.Variable) -> None:
+    """The actual compute kernel. Writes `x` in-place."""
+    n = int(tf.shape(lu)[-1])
 
     # Solve `L y = P b` by forward substitution.
     for i in range(n):
